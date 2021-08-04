2385b64e247d3ca820bc146a9822fb76a966ec4a
erdogan <erdogan.sahin@codeck.com.tr>
2021-08-04 10:18:37 +0300

    remove

--- smart_screen/smart_screen/utils/get_env_variable.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def get_env_variable(name):
-    try:
-        return os.environ[name]
-    except KeyError:
-        message = "Expected environment variable '{}' not set.".format(name)
-        raise Exception(message)
--- smart_screen/smart_screen/utils/is_valid_uuid.py
+++
@@ -1,8 +0,0 @@
-import uuid
-
-def is_valid_uuid(id):
-    try:
-        uuid.UUID(str(id))
-        return True
-    except ValueError:
-        return False
